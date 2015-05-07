d2190bb0cf399f5bf9d34680a86af36e4299ebdb
Jean Niklas L'orange <jeannikl@hypirion.com>
2015-05-08 00:22:19 +0200

    Bump deps.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject lein-survey "1.0.0-SNAPSHOT"
   :description "Survey for Leiningen users"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [incanter/incanter-charts "1.4.1"]
+  :dependencies [[org.clojure/clojure "1.7.0-beta2"]
+                 [incanter/incanter-charts "1.5.6"]
                  [org.clojure/java.jdbc "0.2.1"]
                  [postgresql "9.1-901-1.jdbc4"]
                  [hiccup "1.0.2"]
